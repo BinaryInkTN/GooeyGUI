3bb667347c735870114ba11926d6444c6d072090
Onizuka09 <smokthar925@gmail.com>
2025-03-29 04:10:22 +0100

    fix bug in dropdown menu where if 2 dropdowns are undereach creates undersired effects in the click events

--- examples/b.sh
+++ examples/b.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd .. && cd build && make && sudo make install && cd .. && cd examples && gcc builder_example.c  -o builder_example -L/usr/local/lib -lfreetype -lGooeyGUI -I/usr/local/include/Gooey/  -lm -fsanitize=address,undefined && ./builder_example
+cd .. && cd build && make && sudo make install && cd .. && cd examples && gcc dropdown_example.c  -o dropdown_example -L/usr/local/lib -lfreetype -lGooeyGUI -I/usr/local/include/Gooey/  -lm -fsanitize=address,undefined && ./dropdown_example
---
+++ examples/dropdown_example.c
@@ -0,0 +1,30 @@
+#include <Gooey/gooey.h>
+
+void dropdown_callback(int selected_index)
+{
+    printf("Dropdown selection changed to index: %d\n", selected_index);
+}
+
+int main()
+{
+
+    Gooey_Init();
+    GooeyWindow *win = GooeyWindow_Create("DropDown", 800, 800, true);
+
+    const char *fruits_options[] = {"apple", "banana", "manga", "melon"};
+    const char *quality_options[] = {"Low", "Medium", "High", "Ultra"};
+    const char *color_options[] = {"Red", "Green", "Blue", "Yellow", "Cyan", "Magenta"};
+
+    // Widgets for Options tab
+    GooeyDropdown *fruitsDropdown = GooeyDropdown_Create(130, 30, 150, 30,fruits_options, 4, dropdown_callback);
+    GooeyDropdown *qualityDropdown = GooeyDropdown_Create(30, 130, 150, 30, quality_options, 4, dropdown_callback);
+    GooeyDropdown *colorDropdown = GooeyDropdown_Create(30, 180, 150, 30, color_options, 6, dropdown_callback);
+
+    GooeyWindow_RegisterWidget(win, colorDropdown);
+
+    GooeyWindow_RegisterWidget(win, qualityDropdown);
+    GooeyWindow_RegisterWidget(win, fruitsDropdown);
+    GooeyWindow_Run(1, win);
+    GooeyWindow_Cleanup(1, win);
+    return 0;
+}

1c6d51d48875b0a0863e70e1a07074948004ed35
YASSINE-AA <overflowtn@gmail.com>
2025-03-29 02:41:10 +0100

    GooeyBuilder: canvas implemented

--- examples/builder_example.c
+++ examples/builder_example.c
@@ -26,12 +26,10 @@ int main()
     Gooey_Init();
     GooeyWindow *win = GooeyWindow_Create("Hello World", 800, 600, true);
 
-    GooeyButton *button_0 = GooeyButton_Create("Mokhtar", 277, 113, 100, 30, button_callback);
-    GooeyTextbox *input_1 = GooeyTextBox_Create(105, 114, 100, 30, "Type here", input_callback);
+    GooeyCanvas *canvas_0 = GooeyCanvas_Create(134, 90, 100, 30);
 
     // Register all widgets with the window
-    GooeyWindow_RegisterWidget(win, button_0);
-    GooeyWindow_RegisterWidget(win, input_1);
+    GooeyWindow_RegisterWidget(win, canvas_0);
 
     GooeyWindow_Run(1, win);
     GooeyWindow_Cleanup(1, win);

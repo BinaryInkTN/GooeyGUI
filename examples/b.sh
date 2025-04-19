c69df1e116254a16264edf31ff9fcb13511e2da3
YASSINE-AA <overflowtn@gmail.com>
2025-04-19 22:11:08 +0100

    fixing performance issues on x11

--- examples/b.sh
+++ examples/b.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd .. && cd build && make -j 12 && sudo make install && cd .. && cd examples && gcc IoT_dashboard.c  -o bin/tabs_example -L/usr/local/lib/ -lfreetype -lGooeyGUI -lpaho-mqtt3c -I/usr/local/include/ -I/usr/local/include/Gooey/ -I/usr/local/include/GLPS/ -lGLPS  -lm -fsanitize=address,undefined && ./bin/IoT_dashboard
+cd .. && cd build && make -j 12 && sudo make install && cd .. && cd examples && gcc tabs_example.c  -o bin/tabs_example -L/usr/local/lib/ -lfreetype -lGooeyGUI -lpaho-mqtt3c -I/usr/local/include/ -I/usr/local/include/Gooey/ -I/usr/local/include/GLPS/ -lGLPS  -lm -fsanitize=address,undefined && ./bin/tabs_example

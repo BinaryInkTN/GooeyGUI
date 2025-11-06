d72bd6150cf46944a67b081af601982b057a0f0e
Yassine Ahmed Ali <overflowtn@gmail.com>
2025-11-06 17:23:01 -0500

    added comment to effect example

--- examples/counter.c
+++ examples/counter.c
@@ -1,35 +1,71 @@
-#include "gooey.h"
+#include <Gooey/gooey.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int counter = 0;
-GooeyLabel *counterLabel;
+void increment_fn(void *user_data);
+void update_ui(void *context);
 
-void onIncrementClick()
+typedef struct
 {
-    counter++;
-    char text[20];
-    snprintf(text, sizeof(text), "Counter: %d", counter);
-    GooeyLabel_SetText(counterLabel, text);
+    GooeyLabel *label;
+    GooeySignal *signal;
+} AppData;
+
+void increment_fn(void *user_data)
+{
+    AppData *app = (AppData *)user_data;
+
+    int *count = (int *)GooeySignal_GetValue(app->signal);
+
+    (*count)++;
+
+    GooeySignal_SetValue(app->signal, count);
+}
+
+void update_ui(void *context)
+{
+    AppData *app = (AppData *)context;
+    int *count = (int *)GooeySignal_GetValue(app->signal);
+
+    char text[64];
+    snprintf(text, sizeof(text), "Counter: %d", *count);
+
+    GooeyLabel_SetText(app->label, text);
 }
 
 int main()
 {
-    Gooey_Init(GLPS);
+    // Init Gooey
+    Gooey_Init();
 
-    GooeyWindow win = GooeyWindow_Create("Counter App", 300, 130, true);
+    GooeyWindow *window = GooeyWindow_Create("Simple Counter", 400, 300, true);
 
-    GooeyLayout *layout = GooeyLayout_Create(&win, LAYOUT_VERTICAL, 10, 50, 280, 180);
+    GooeyLayout *vertical_layout = GooeyLayout_Create(LAYOUT_GRID, 0, 0, 400, 300);
 
-    counterLabel = GooeyLabel_Add(&win, "Counter: 0", 0.27f, 0, 0);
-    GooeyLayout_AddChild(layout, counterLabel);
+    GooeySignal *signal = GooeySignal_Create(sizeof(int), 0);
+    int initial_value = 10;
+    GooeySignal_SetValue(signal, &initial_value);
+    GooeyLabel *label = GooeyLabel_Create("Counter: 0", 0.3f, 0, 0);
 
-    GooeyButton *incrementButton = GooeyButton_Add(&win, "Increment", 0, 0, 100, 30, onIncrementClick);
-    GooeyLayout_AddChild(layout, incrementButton);
+    AppData *app = malloc(sizeof(AppData));
+    app->label = label;
+    app->signal = signal;
 
-    GooeyLayout_Build(layout);
+    GooeyButton *button = GooeyButton_Create("Click Me!", 0, 0, 100, 30, increment_fn, app);
 
-    GooeyWindow_Run(1, &win);
-    GooeyWindow_Cleanup(1,&win);
+    GooeyReactEffect *effect_hook = GooeyReact_CreateEffect(update_ui, app);
 
+    GooeyLayout_AddChild(window, vertical_layout, label);
+    GooeyLayout_AddChild(window, vertical_layout, button);
+
+    GooeyWindow_RegisterWidget(window, vertical_layout);
+
+    GooeyWindow_Run(1, window);
+
+    free(app);
+    GooeyWindow_Cleanup(1, window);
+    GooeySignal_Destroy(signal);
+    GooeyReact_DestroyEffect(effect_hook);
     return 0;
 }
--- examples/example_test.h
+++
@@ -1,6 +0,0 @@
-#ifndef EXAMPLE_TEST_H
-#define EXAMPLE_TEST_H
-
-void draw_example_test();
-
-#endif
